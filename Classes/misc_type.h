f238fc60aa5889b5fb01c5fb3096f13e77c58614
Ming Ming <mint2648@gmail.com>
2015-03-10 03:45:28 +0800

    Add commonly used types: Uint & Byte

---
+++ Classes/misc_type.h
@@ -0,0 +1,13 @@
+/*
+ * misc_type.h
+ *
+ * Copyright (C) 2014-2015  Ming Tsang, Nathan Ng
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <cstdint>
+
+typedef unsigned int Uint;
+typedef uint8_t Byte;
