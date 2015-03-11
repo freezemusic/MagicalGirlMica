e6c12f92377db6c851827d586c667fe386449bb3
Ming Ming <mint2648@gmail.com>
2015-03-11 17:44:18 +0800

    Add a joystick that work with an invisible on-screen area

---
+++ src/area_joystick.cpp
@@ -0,0 +1,121 @@
+/*
+ * area_joystick.cpp
+ *
+ * Copyright (C) 2014-2015  Ming Tsang, Nathan Ng
+ * Refer to LICENSE for details
+ */
+
+#include <2d/CCLayer.h>
+#include <2d/CCNode.h>
+#include <base/CCEventDispatcher.h>
+#include <base/CCEventListenerTouch.h>
+
+#include <libutils/type/coord.h>
+#include <libutils/type/rect.h>
+#include <libutils/type/rect_utils.h>
+
+#include "area_joystick.h"
+#include "log.h"
+#include "res_manager.h"
+
+using namespace cocos2d;
+using namespace utils::type;
+
+#define NS_TAG "mica::"
+#define TAG NS_TAG "AreaJoystick::"
+
+namespace mica
+{
+
+AreaJoystick::AreaJoystick()
+		: m_x(0),
+		  m_y(0)
+{
+	setGood(false);
+}
+
+AreaJoystick::AreaJoystick(const Config &config)
+		: AreaJoystick()
+{
+	init(config);
+}
+
+AreaJoystick::~AreaJoystick()
+{
+	uninit();
+}
+
+bool AreaJoystick::init(const Config &config)
+{
+	uninit();
+
+	m_rect = config.rect;
+	setGood(initView() && initListeners());
+	return *this;
+}
+
+bool AreaJoystick::initView()
+{
+	Node *view = LayerColor::create(Color4B(0, 0, 0, 0), m_rect.size.w,
+			m_rect.size.h);
+	if (!view)
+	{
+		LOG_E(TAG "initView", "Failed while LayerColor::create");
+		return false;
+	}
+
+	view->setAnchorPoint({0.0f, 0.0f});
+	view->setPosition(m_rect.coord.x, m_rect.coord.y);
+	view->setVisible(false);
+	setView(view);
+	return true;
+}
+
+bool AreaJoystick::initListeners()
+{
+	auto *listener = EventListenerTouchOneByOne::create();
+	listener->setSwallowTouches(true);
+
+	auto reset_touch = [this](Touch*, Event*)
+			{
+				m_x = 0;
+				m_y = 0;
+				invokeOnMoveListeners();
+			};
+	listener->onTouchBegan = [this, reset_touch](Touch *touch, Event*)
+			{
+				const Vec2 &pt = touch->getStartLocation();
+				if (RectUtils::IsInsidePx(m_rect, Coord(pt.x, pt.y)))
+				{
+					reset_touch(touch, nullptr);
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			};
+	listener->onTouchMoved = [this](Touch *touch, Event*)
+			{
+				const Vec2 &coord = touch->getLocation()
+						- touch->getStartLocation();
+				m_x = coord.x;
+				m_y = coord.y;
+				invokeOnMoveListeners();
+				LOG_V(TAG "initListeners(onTouchMoved)",
+						utils::str::StrUtils::Concat(m_x, ", ", m_y));
+			};
+	listener->onTouchEnded = reset_touch;
+	listener->onTouchCancelled = reset_touch;
+
+	getView()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(
+			listener, getView());
+	return true;
+}
+
+void AreaJoystick::uninit()
+{
+	setView(nullptr);
+}
+
+}
---
+++ src/area_joystick.h
@@ -0,0 +1,63 @@
+/*
+ * area_joystick.h
+ *
+ * Copyright (C) 2014-2015  Ming Tsang, Nathan Ng
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <libutils/type/rect.h>
+
+#include "on_screen_joystick.h"
+
+namespace mica
+{
+
+/**
+ * A joystick that covers an invisible rectangular area. The center point would
+ * be the point you dropped your finger
+ */
+class AreaJoystick : public OnScreenJoystick
+{
+public:
+	struct Config
+	{
+		utils::type::Rect rect;
+	};
+
+	explicit AreaJoystick(const Config &config);
+	virtual ~AreaJoystick();
+
+	int getX() const override
+	{
+		return m_x;
+	}
+
+	int getY() const override
+	{
+		return m_y;
+	}
+
+protected:
+	AreaJoystick();
+
+	bool init(const Config &config);
+	void uninit();
+
+	const utils::type::Rect& getRect() const
+	{
+		return m_rect;
+	}
+
+private:
+	bool initView();
+	bool initListeners();
+
+	utils::type::Rect m_rect;
+
+	int m_x;
+	int m_y;
+};
+
+}
