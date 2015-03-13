d2b192bcdb9c2a88188f261a379ae103a7c19ac0
Ming Ming <mint2648@gmail.com>
2015-03-14 07:59:28 +0800

    Cleanup code

--- src/AppDelegate.cpp
+++ src/AppDelegate.cpp
@@ -2,79 +2,75 @@
 #include "cocos2d_wrapper.h"
 
 #include "AppDelegate.h"
+#include "log.h"
 #include "res_manager.h"
-#include "World.h"
+#include "test_stage_scene.h"
 
-USING_NS_CC;
+using namespace cocos2d;
 using namespace std;
+
+#define NS_TAG "mica::"
+#define TAG NS_TAG "AppDelegate::"
 
 namespace mica
 {
 
-AppDelegate::AppDelegate() {
-
-}
-
-AppDelegate::~AppDelegate()
-{
-}
-
 void AppDelegate::initGLContextAttrs()
 {
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
-
-    GLView::setGLContextAttrs(glContextAttrs);
+	GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+	GLView::setGLContextAttrs(glContextAttrs);
 }
 
-bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-        glview = GLViewImpl::create("Cpp Empty Test");
+bool AppDelegate::applicationDidFinishLaunching()
+{
+	// initialize director
+	auto *director = Director::getInstance();
+	GLView *glview = director->getOpenGLView();
+	if (!glview)
+	{
+		glview = GLViewImpl::create("Cpp Empty Test");
 #if CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 \
 		|| CC_TARGET_PLATFORM == CC_PLATFORM_LINUX \
 		|| CC_TARGET_PLATFORM == CC_PLATFORM_MAC
-        glview->setFrameSize(ResManager::getDesignW(), ResManager::getDesignH());
+		glview->setFrameSize(ResManager::getDesignW(), ResManager::getDesignH());
 #endif
-        director->setOpenGLView(glview);
-    }
-
-	//cocos2d::Size designResolutionSize = cocos2d::Size(1024, 480);
-
-    director->setOpenGLView(glview);
+		director->setOpenGLView(glview);
+	}
 	glview->setDesignResolutionSize(ResManager::getDesignW(),
 			ResManager::getDesignH(), ResolutionPolicy::SHOW_ALL);
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+	director->setDisplayStats(true);
+	// set FPS
+	director->setAnimationInterval(1.0 / 60);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+	// Load the test scene
+	auto *scene = TestStageScene::create();
+	if (!scene)
+	{
+		LOG_E(TAG "applicationDidFinishLaunching",
+				"Failed while creating TestStageScene");
+		return false;
+	}
 
-    // create a scene. it's an autorelease object
-    auto scene = World::createScene();
-
-    // run
-    director->runWithScene(scene);
-
-    return true;
+	// run
+	director->runWithScene(scene);
+	return true;
 }
 
-// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
-void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
+void AppDelegate::applicationDidEnterBackground()
+{
+	Director::getInstance()->stopAnimation();
 
-    // if you use SimpleAudioEngine, it must be pause
-    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+	// if you use SimpleAudioEngine, it must be pause
+	// SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
 }
 
-// this function will be called when the app is active again
-void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+void AppDelegate::applicationWillEnterForeground()
+{
+	Director::getInstance()->startAnimation();
 
-    // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+	// if you use SimpleAudioEngine, it must resume here
+	// SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
 }
 
 }
--- src/AppDelegate.h
+++ src/AppDelegate.h
@@ -1,41 +1,20 @@
 #pragma once
 
+#include <memory>
+
 #include <platform/CCApplication.h>
 
 namespace mica
 {
 
-/**
-@brief    The cocos2d Application.
-
-The reason for implement as private inheritance is to hide some interface call by Director.
-*/
 class AppDelegate : public cocos2d::Application
 {
 public:
-    AppDelegate();
-    virtual ~AppDelegate();
+	void initGLContextAttrs() override;
 
-    virtual void initGLContextAttrs();
-
-    /**
-    @brief    Implement Director and Scene init code here.
-    @return true    Initialize success, app continue.
-    @return false   Initialize failed, app terminate.
-    */
-    virtual bool applicationDidFinishLaunching();
-
-    /**
-    @brief  The function be called when the application enter background
-    @param  the pointer of the application
-    */
-    virtual void applicationDidEnterBackground();
-
-    /**
-    @brief  The function be called when the application enter foreground
-    @param  the pointer of the application
-    */
-    virtual void applicationWillEnterForeground();
+	bool applicationDidFinishLaunching() override;
+	void applicationDidEnterBackground() override;
+	void applicationWillEnterForeground() override;
 };
 
 }
