1038e5aae36e576a38945255fafd5a7b73b2fc2f
Ming Ming <mint2648@gmail.com>
2015-03-15 03:35:48 +0800

    Squashed 'external/libutils/' changes from 46f0901..ef45192
    ef45192 Add alias for the BE/LE to host conversion
    b8c7797 Add comment
    5597a11 Add fn to convert half rotation to full rotation
    015062d Add fn to get angle between a vector and the x-axis
    dbc0f09 Add pi
    0c20304 Delete constructor
    
    git-subtree-dir: external/libutils
    git-subtree-split: ef45192f48eea0e35dcc44e09bfc99772b77308c

--- inc/libutils/math/math_utils.h
+++ inc/libutils/math/math_utils.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <type_traits>
+
 namespace utils
 {
 namespace math
@@ -20,6 +22,60 @@ public:
 
 	template<typename T>
 	static T Clamp(const T &min, const T &val, const T &max);
+
+	template<typename T>
+	static constexpr T Pi();
+
+	/**
+	 * Return the angle between a vector, defined by @a x and @a y, and the x
+	 * axis, [-180, 180]
+	 *
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	template<typename T>
+	static typename std::enable_if<std::is_arithmetic<T>::value, float>::type
+	GetAngleFromX(const T x, const T y);
+	/**
+	 * Return the angle between a vector @a vec and the x axis, [-180, 180]
+	 *
+	 * @note @a vec is required to provide access to x and y
+	 * @param pt
+	 * @return
+	 */
+	template<typename T>
+	static float GetAngleFromX(const T &vec)
+	{
+		return GetAngleFromX(vec.x, vec.y);
+	}
+	/**
+	 * Return the angle between a vector @a vec and the x axis, at the point
+	 * @a origin, [-180, 180]
+	 *
+	 * @note @a vec is required to provide access to x and y
+	 * @param origin
+	 * @param pt
+	 * @return
+	 */
+	template<typename T>
+	static typename std::enable_if<!std::is_arithmetic<T>::value, float>::type
+	GetAngleFromX(const T &origin, const T &vec)
+	{
+		return GetAngleFromX(vec.x - origin.x, vec.y - origin.y);
+	}
+
+	/**
+	 * Convert a [-180, 180] half rotation to a [0, 360] full rotation
+	 *
+	 * @param angle
+	 * @return
+	 */
+	template<typename T>
+	T HalfToFullRotation(const T angle)
+	{
+		return ((angle >= 0) ? angle : (angle + 360));
+	}
 };
 
 }
--- inc/libutils/platform/endian_utils.h
+++ inc/libutils/platform/endian_utils.h
@@ -24,15 +24,68 @@ public:
 		return !IsBigEndian();
 	}
 
+	/**
+	 * Change the endianness of a 2-byte or 4-byte data. Internally it simply
+	 * delegate the call to one of the corresponding Translate* method
+	 *
+	 * @param from
+	 * @return
+	 */
 	template<typename T>
 	static T Translate(const T from);
+	/**
+	 * Change the endianness of a 2-byte data
+	 *
+	 * @param from
+	 * @return
+	 */
 	static uint16_t Translate16(const uint16_t from);
+	/**
+	 * Change the endianness of a 4-byte data
+	 *
+	 * @param from
+	 * @return
+	 */
 	static uint32_t Translate32(const uint32_t from);
 
+	/**
+	 * Covert data from host endian to big endian
+	 *
+	 * @param host
+	 * @return
+	 */
 	template<typename T>
 	static T HostToBe(const T host);
+	/**
+	 * Covert data from big endian to host endian
+	 *
+	 * @param be
+	 * @return
+	 */
+	template<typename T>
+	static T BeToHost(const T be)
+	{
+		return HostToBe(be);
+	}
+	/**
+	 * Covert data from host endian to little endian
+	 *
+	 * @param host
+	 * @return
+	 */
 	template<typename T>
 	static T HostToLe(const T host);
+	/**
+	 * Covert data from little endian to host endian
+	 *
+	 * @param le
+	 * @return
+	 */
+	template<typename T>
+	static T LeToHost(const T le)
+	{
+		return HostToLe(le);
+	}
 };
 
 }
