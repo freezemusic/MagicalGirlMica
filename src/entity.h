6185d9cd8b0e3e66d2215d240ced2a634e937bc8
Ming Ming <mint2648@gmail.com>
2015-04-01 03:37:46 +0800

    Entity no longer holds components directly

--- src/entity.h
+++ src/entity.h
@@ -19,24 +19,27 @@ namespace mica
 class Entity final
 {
 public:
-	Entity();
+	explicit Entity(const Uint id)
+			: m_id(id)
+	{}
 
 	operator bool()
 	{
-		return (m_is_good && !m_components.empty());
+		return m_is_good;
 	}
-
-	void addComponent(std::unique_ptr<com::Component> &&com);
-	void removeComponent(const Uint com_id);
-	com::Component* getComponent(const Uint com_id);
 
 	void setGood(const bool flag)
 	{
 		m_is_good = flag;
 	}
 
+	Uint getId() const
+	{
+		return m_id;
+	}
+
 private:
-	std::unordered_map<Uint, std::unique_ptr<com::Component>> m_components;
+	Uint m_id;
 	bool m_is_good = true;
 };
 
